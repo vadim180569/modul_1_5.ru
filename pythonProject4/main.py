d2c5401cf7ae2d2ff2fba8a37b93bf2c3c9514a5
vadim180569 <vadim9771505001@gmail.com>
2024-11-01 15:55:45 +0300

    Add files via upload

---
+++ pythonProject4/main.py
@@ -0,0 +1,12 @@
+#immutable_var = 1 , 2 , True , "horse"
+#print(immutable_var)
+#immutable_var[3] = "donkey"
+#print(immutable_var)#кортеж(tuple)-неизменяемый тип
+# данных и не поддерживает обращение по элементам.
+mutable_list = [1 , 2 , True , "giraffe"]
+print(mutable_list[3])
+mutable_list[3] = "donkey"
+print(mutable_list)
+
+
+
